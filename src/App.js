bdb2f47fdbd68af6215ee340c53a8ce226737a9a
Marten Wallewein <martenwallewein@web.de>
2019-12-29 15:58:42 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import FsLightbox from "fslightbox-react";
+import {Col, Row} from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import _ from 'lodash';
 
 function App() {
+
+  const [images, setImages] = useState([]);
+  const [toggler, setToggler] = useState(false);
+  const [sourceIndex, setSourceIndex] = useState(0);
+
+  useEffect(() => {
+    const imgs = _.times(100).map((num) => {
+      return {
+        id: num,
+        iconSrc: `https://place-hold.it/600?v=${num}`,
+        scaledSrc: `https://miro.medium.com/max/4320/0*QNdQhs_T3ffa6B0m.jpeg?v=${num}`,
+      };
+    });
+
+    setImages(imgs);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row className={"PreviewContainerContent"}>
+        {
+            images.map((img, index) => {
+                return (
+                    <Col
+                        className={"GalleryImageCol"}
+                        {...{md: 4, xs: 12, sm: 6, xl: 3, xxl: 2}}
+                        key={`Galleryimg-${index}`}
+                        widths={["xs", "sm", "md", "lg", "xl", "xxl"]}
+                        onClick={() => {
+                            setSourceIndex(index);
+                            setToggler(!toggler);
+                        }}
+                    >
+                        <img src={img.iconSrc} key={img.id} className={"GalleryImage"} alt="icon"/>
+                    </Col>
+                );
+
+            })
+        }
+    </Row>
+     <FsLightbox
+        toggler={toggler}
+        sourceIndex={sourceIndex}
+        type={"image"}
+        sources={images.map((img) => (img.scaledSrc))}
+        thumbs={images.map((img) => img.iconSrc)}
+        showThumbsOnMount={true}
+        key={images}
+    />
     </div>
   );
 }
